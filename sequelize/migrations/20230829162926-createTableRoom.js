f8d3d943c54c4e21dd5b645f32f0e2a039a78a42
RoxasDavega <bintang.magician@gmail.com>
2023-08-30 19:15:28 +0700

    udpate migrations table room

--- sequelize/migrations/20230829162926-createTableRoom.js
+++ sequelize/migrations/20230829162926-createTableRoom.js
@@ -14,6 +14,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
+        primaryKey: true,
       },
       name: {
         type: Sequelize.STRING,
